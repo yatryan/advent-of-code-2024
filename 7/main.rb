569d80f151ae594a68829f36cb91ecaba6a40d45
Taylor Ryan <taylor@orbweaver.com>
2024-12-12 23:43:56 -0500

    day 7 part 1

---
+++ 7/main.rb
@@ -0,0 +1,39 @@
+file = "input.txt"
+
+equations = []
+
+File.readlines(File.join(__dir__, file)).each do |line|
+  answer, numbers = line.strip.split(':')
+  equations << {
+    answer: answer.to_i,
+    numbers: numbers.split(' ').map(&:to_i)
+  }
+end
+
+total_calibration = 0
+
+def test_calc(answer, current, numbers)
+  return answer == current if numbers.count.zero?
+
+  cur_num = numbers.shift
+
+  # check plus
+  plus_answer = test_calc(answer, current + cur_num, numbers.dup)
+  return true if plus_answer
+
+  # check multiply
+  mutl_answer = test_calc(answer, current * cur_num, numbers.dup)
+  return true if mutl_answer
+
+  false
+end
+
+equations.each do |equation|
+
+  is_valid = test_calc(equation[:answer], 0, equation[:numbers])
+
+  total_calibration += equation[:answer] if is_valid
+end
+
+
+puts total_calibration
